7fc42772eb8a828a23211ef298293c94a8f787b9
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2019-05-29 15:36:01 -0700

    Init Working Commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+// core modules - built in no need to install
+const path = require('path')
+
+// npm modules
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const port = 3000
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPATH = path.join(__dirname, '../client/views')
+const partialsPATH = path.join(__dirname, '../client/partials')
+
+// set up handlebars and 'views' location path
+app.set('view engine', 'hbs')
+app.set('views', viewsPATH)
+hbs.registerPartials(partialsPATH)
+
+// access to assets folder - css, images, js etc
+app.use(express.static(publicDirectory))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Dwyan Alford'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Dwyan Alford'
+    })
+})
+
+app.get('/events', (req, res) => {
+    res.render('events', {
+        title: 'About Me',
+        name: 'Dwyan Alford'
+    })
+})
+
+// should be last after all other routes defined
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'About Me',
+        name: 'Dwyan Alford'
+    })
+})
+
+// start the server
+app.listen(port, () => console.log(`Web app listening on port ${port}!`))
